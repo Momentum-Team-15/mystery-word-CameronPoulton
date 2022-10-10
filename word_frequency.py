db9f47e1466867b589d874716d2c16e2b998bb3e
Cameron Poulton <Crp2108@hotmail.com>
2022-10-10 09:18:15 -0400

    complete

--- word_frequency.py
+++ word_frequency.py
@@ -13,23 +13,23 @@ def print_word_freq(file):
 # Read the file
         text_content = file.read()
 # Remove punctuation
-    for char in text_content:
+    for char in text:
         if char in PUNCTUATION:
-            text_content = text_content.replace(char, "")
-            return text_content
+            text = text.replace(char, "")
+            return text
 # Split the content to separate words into a list 
-    text_content = text_content.split()
+    text = text.split()
 # Change all the words to lower case
-    text_content = [word.lower() for word in text_content]
+    text = [word.lower() for word in text]
 # Take out all the 'STOP' words
-    for word in text_content:
+    for word in text:
         if word in STOP_WORDS:
-            text_content.remove(word)
+            text.remove(word)
 # Separate the word and the counter into dictionary's 
     counter_dictionary = ()
     word_dictionary = ()
 # define 'counter' and 'word' with 'integer' or '*' 
-    for word in text_content:
+    for word in text:
         if word in counter_dictionary and word_dictionary:
             counter_dictionary[word] += 1
             word_dictionary[word] += '*'
